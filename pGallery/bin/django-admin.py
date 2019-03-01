c28ce82a1f8b09306237dc6cb8adebb35ce8de0a
jinka <voiphandy@gmail.com>
2019-03-01 10:51:08 +0300

    Initial commit

---
+++ pGallery/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/daud/Desktop/moringa-school/Core/Python/Django/WEEK1-IP/personal-Gallery/pGallery/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
